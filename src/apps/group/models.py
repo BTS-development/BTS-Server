293db98e9398e879bf0e205ec173da2aaa59c17a
seojinsarejj <s19009@naver.com>
2020-08-09 22:41:19 +0900

    [UPDATE] Set unique key on Group.code

--- src/apps/group/models.py
+++ src/apps/group/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Group(models.Model):
     name = models.CharField(max_length=255)
     owner = models.ForeignKey("user.User",on_delete=models.CASCADE)
-    code = models.CharField(max_length=255)
+    code = models.CharField(max_length=255,unique=True)
     members = models.ManyToManyField("user.User",through="LinkedUserGroup")
     created_at = models.DateField(auto_now_add=True)
 
