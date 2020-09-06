9f2abc9bd12505dcce44c81f7483acd61f1e1a79
hanbin8269 <hanbin8269@gmail.com>
2020-09-06 23:14:37 +0900

    add settings.__init__ deploy

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -3,4 +3,4 @@ import os
 # DJANGO_SETTINGS_MODULE 환경변수가 지정되지 않거나 config.settings인 경우
 SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE")
 if not SETTINGS_MODULE or SETTINGS_MODULE == "config.settings":
-    from .local import *
+    from .deploy import *
