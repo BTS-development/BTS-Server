4216e6f5c9f7c057624fba79511e51a645465990
hanbin8269 <hanbin8269@gmail.com>
2020-09-14 17:31:31 +0900

    start to write wsgi code

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -2,6 +2,7 @@ from .base import *
 
 DEBUG = True
 WSGI_APPLICATION = "config.wsgi.application"
+ALLOWED_HOSTS += "*"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
